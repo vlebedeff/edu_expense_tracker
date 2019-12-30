33dae74d7f2046412d53110c94394b6e91cc00d2
Vlad Lebedev <vladlebedeff@gmail.com>
2019-12-30 15:09:23 -0800

    Log db example starts and endings

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -1,5 +1,8 @@
 RSpec.configure do |c|
   c.before(:suite) do
+    FileUtils.mkdir_p('log')
+    require 'logger'
+    DB.loggers << Logger.new('log/sequel.log')
     Sequel.extension :migration
     Sequel::Migrator.run(DB, 'db/migrations')
     DB[:expenses].truncate
@@ -8,5 +11,11 @@ RSpec.configure do |c|
 
   c.around(:example, :db) do |example|
     DB.transaction(rollback: :always) { example.run }
+  end
+
+  c.around(:example, :db) do |example|
+    DB.log_info("Starting example: #{example.full_description}")
+    example.run
+    DB.log_info("Ending example: #{example.full_description}")
   end
 end
